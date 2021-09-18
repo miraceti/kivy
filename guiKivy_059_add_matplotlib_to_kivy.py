bffb8e07738babbdf4a94f6eaf7c4e147cf55db2
Rico Miraceti <elecam@gmail.com>
2021-09-18 20:53:57 +0200

    save_matplotlibplt1

---
+++ guiKivy_059_add_matplotlib_to_kivy.py
@@ -0,0 +1,38 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.uix.floatlayout import FloatLayout
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+import matplotlib.pyplot as plt
+
+# define what we want to graph
+x = [1,2,3,4,5]
+y = [5,12,6,9,15]
+
+plt.plot(x,y)
+plt.ylabel("Y axes")
+plt.xlabel("X axes")
+
+class Matty(FloatLayout):
+    def __init__ (self, **kwargs):
+        super().__init__(**kwargs)
+        
+        box = self.ids.box
+        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        
+    def save_it(self):
+        name = self.ids.namer.text
+        if name:
+            plt.savefig(name)
+            
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style= "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+               
+        Builder.load_file('kivy059.kv')
+        
+        return Matty()
+      
+          
+MainApp().run()
