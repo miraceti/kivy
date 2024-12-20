f8eacd49764a910f6fd3d9d414f614b3deffc666
miraceti <elecam@gmail.com>
2024-12-20 18:21:54 +0100

    006

---
+++ KIVY_to_APK/kivy_pl_005/main.py
@@ -0,0 +1,90 @@
+# Sample Python application demonstrating the 
+# working of PageLayout in Kivy with some features
+
+import kivy 
+
+# base Class of your App inherits from the App class. 
+# app:always refers to the instance of your application 
+from kivy.app import App 
+
+# The PageLayout class is used to create
+# a simple multi-page layout,
+# in a way that allows easy flipping from
+# one page to another using borders.
+from kivy.uix.pagelayout import PageLayout
+
+# creates the button in kivy 
+# if not imported shows the error 
+from kivy.uix.button import Button
+
+
+# The Utils module provides a selection of general utility
+# functions and classes that may be useful for various applications.
+# These include maths, color, algebraic and platform functions.
+# Here we are using color from the module
+# By get_color_from_hex
+# Transform a hex string color to a kivy Color.
+from kivy.utils import get_color_from_hex
+
+class PageLayout(PageLayout):
+	"""
+	Define class PageLayout here
+	"""
+
+	def __init__(self):
+		
+		# The super function in Python can be
+		# used to gain access to inherited methods
+		# which is either from a parent or sibling class.
+		super(PageLayout, self).__init__()
+
+		# creating buttons on different pages
+
+		# Button 1 or Page 1
+		btn1 = Button(text ='Page 1')
+		# Adding Colour to page
+		# Here we are using colour from
+	
+		btn1.background_color = get_color_from_hex('#FF0000')
+
+		btn2 = Button(text ='Page 2')
+		btn2.background_color = get_color_from_hex('#00FF00')
+
+		btn3 = Button(text ='Page 3')
+		btn3.background_color = get_color_from_hex('#0000FF')
+
+		btn4 = Button(text ='Page 4')
+		btn4.background_color = get_color_from_hex('#FFFF00')
+		
+		btn5 = Button(text ='Page 5')
+		btn5.background_color = get_color_from_hex('#FF00FF')
+		
+# adding button on the screen
+		# by add widget method
+		self.add_widget(btn1)
+		
+		self.add_widget(btn2)
+		
+		self.add_widget(btn3)
+		
+		self.add_widget(btn4)
+		
+		self.add_widget(btn5)
+
+
+# creating the App class
+class Page_LayoutApp(App):
+	"""
+	App class here
+	"""
+
+	def build(self):
+		"""
+		build function here
+		"""
+		return PageLayout()
+
+
+# Run the App
+if __name__ == '__main__':
+	Page_LayoutApp().run()
---
+++ KIVY_to_APK/kivy_pl_007/main.py
@@ -0,0 +1,81 @@
+"""Basic example for kivy_matplotlib_widget
+
+    This example don't use kivy_matplotlib_widget module in case you want to
+    incorporate some widgets in your project and you want to customized stuffs.
+    If you don't need custom used, please install the module with:
+        
+        pip install kivy-matplotlib-widget
+    
+Note:
+    MatplotFigure is used when you have only 1 axis with lines only.
+    
+"""
+from kivy_matplotlib_widget.uix.graph_widget import MatplotFigure
+from kivy.utils import platform
+from kivy.config import Config
+
+#avoid conflict between mouse provider and touch (very important with touch device)
+#no need for android platform
+if platform != 'android':
+    Config.set('input', 'mouse', 'mouse,disable_on_activity')
+else:
+    #for android, we remove mouse input to not get extra touch 
+    Config.remove_option('input', 'mouse')
+
+from kivy.lang import Builder
+from kivy.app import App
+
+import matplotlib.pyplot as plt
+#:import MatplotFigure graph_widget
+KV = '''
+
+
+Screen
+    figure_wgt:figure_wgt
+    BoxLayout:
+        orientation:'vertical'
+        BoxLayout:
+            size_hint_y:0.2
+            Button:
+                text:"home"
+                on_release:app.home()
+            ToggleButton:
+                group:'touch_mode'
+                state:'down'
+                text:"pan" 
+                on_release:
+                    app.set_touch_mode('pan')
+                    self.state='down'
+            ToggleButton:
+                group:'touch_mode'
+                text:"zoom box"  
+                on_release:
+                    app.set_touch_mode('zoombox')
+                    self.state='down'                
+        MatplotFigure:
+            id:figure_wgt
+'''
+
+
+class Test(App):
+    lines = []
+
+    def build(self):  
+        self.screen=Builder.load_string(KV)
+        return self.screen
+
+    def on_start(self, *args):
+        fig, ax1 = plt.subplots(1, 1)
+
+        ax1.plot([0,1,2,3,4], [1,2,8,9,4],label='line1')
+        ax1.plot([2,8,10,15], [15,0,2,4],label='line2')
+        
+        self.screen.figure_wgt.figure = fig
+
+    def set_touch_mode(self,mode):
+        self.screen.figure_wgt.touch_mode=mode
+
+    def home(self):
+        self.screen.figure_wgt.home()
+        
+Test().run()
