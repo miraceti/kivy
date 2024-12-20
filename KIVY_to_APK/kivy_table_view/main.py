3464f1880cb059783d17b3cf4a09dd2b3347ecbc
miraceti <elecam@gmail.com>
2024-12-20 19:16:16 +0100

    table_view

---
+++ KIVY_to_APK/kivy_table_view/main.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+
+from table import TableView, TableColumn
+
+class TableApp(App):
+    def build(self):
+        # create a default grid layout with custom width/height
+        table = TableView(size=(500,320),
+                pos_hint={'x':0.1, 'center_y':.5})
+        # columns
+        table.add_column(TableColumn("Col1", key="1", hint_text='0'))
+        table.add_column(TableColumn("Col2", key="2", hint_text='0'))
+        # content
+        for i in range(30):
+            row = {'1': str(2*i+0), '2': str(2*i+1)}
+            table.add_row(row)
+        return table
+
+if __name__ == '__main__':
+    TableApp().run()
