6e8671ef0740ab8044107ba05263754a8bac59e7
miraceti <elecam@gmail.com>
2024-12-21 16:22:22 +0100

    table001

--- KIVY_to_APK/kivy__scrollview/main.py
+++ KIVY_to_APK/kivy__scrollview/main.py
@@ -1,13 +1,16 @@
 from kivymd.app import MDApp
-from kivy.uix.floatlayout import floatlayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 from kivy.core.window import Window
 
 Window.size = 360, 640
-class Layout_(floatlayout):
+class Layout_(FloatLayout):
     pass
 
 class Main(MDApp):
     def build(self):
         Builder.load_file("ui.kv")
         return Layout_()
+    
+
+Main().run()
